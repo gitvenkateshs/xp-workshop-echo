ae561cfa40773eba0e9c322dacfef1f517ce0493
deekkuma <deekkuma@DEEKKUMA-7REB2.cisco.com>
2015-10-06 17:37:37 +0530

    Power off included

--- EchoApache/src/com/echo/server/ChannelSwitching.java
+++ EchoApache/src/com/echo/server/ChannelSwitching.java
@@ -1,6 +1,12 @@
 package com.echo.server;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,8 +31,27 @@ public class ChannelSwitching extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String channelNumber = request.getParameter("channelNumber");
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String channelNumber = request.getParameter("ChannelNo");
+		System.out.println(channelNumber);
+		String json = getChannelNumber(channelNumber);
+		System.out.println(json);
+		PrintWriter out = response.getWriter();
+		//out.write(json);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	private String getChannelNumber(String channelNumber) {
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			System.out.println(File.separator+"CONFIG"+File.separator+"channel.properties");
+			input = new FileInputStream(File.separator+"CONFIG"+File.separator+"channel.properties");
+			prop.load(input);
+			System.out.println(prop.getProperty(channelNumber.toString()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return prop.getProperty(channelNumber.toString());
 	}
 
 	/**
