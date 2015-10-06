e3d711d8574be87487c8154f944b2d4e5b32d295
deekkuma <deekkuma@DEEKKUMA-7REB2.cisco.com>
2015-10-06 15:21:44 +0530

    changed id

--- EchoApache/src/com/echo/server/ChannelSwitching.java
+++ EchoApache/src/com/echo/server/ChannelSwitching.java
@@ -25,6 +25,7 @@ public class ChannelSwitching extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String channelNumber = request.getParameter("channelNumber");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
