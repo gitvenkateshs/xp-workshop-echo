5d7926f27fca5c9acd2cd430893940799d7a4f75
deekkuma <deekkuma@DEEKKUMA-7REB2.cisco.com>
2015-10-07 10:50:10 +0530

    added jar

--- EchoApache/test/com/echo/test/ChannelSwitchingTest.java
+++ EchoApache/test/com/echo/test/ChannelSwitchingTest.java
@@ -5,27 +5,32 @@ import static org.junit.Assert.assertEquals;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
-
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import com.echo.server.ChannelSwitching;
 
 public class ChannelSwitchingTest {
 	private static ChannelSwitching chSw;
 	
-
+	private static MockHttpServletRequest request;
+    private static MockHttpServletResponse response;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		chSw=new ChannelSwitching();
-		MockitoAnnotations.initMocks(chSw);
+		request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
 	
 	}
 
 	@Test
 	public void getChannel_should_return_channel_number_for_valid_input() {
 		
-		 
-
+		//request.addParameter();
 		//assertEquals("Pogo", chSw.getChannel("3"));
 		//assertEquals("Pogo", chSw.doGet(request,response));
+		 //chSw.doPost(request, response);
+
+	        assertEquals("text/html", response.getContentType());
 
 	}
 	//@Test
