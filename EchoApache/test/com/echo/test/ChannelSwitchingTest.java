26817105fa1d8743c4ab4d30dba3302dfec13226
deekkuma <deekkuma@DEEKKUMA-7REB2.cisco.com>
2015-10-07 09:08:50 +0530

    Changed test

--- EchoApache/test/com/echo/test/ChannelSwitchingTest.java
+++ EchoApache/test/com/echo/test/ChannelSwitchingTest.java
@@ -2,6 +2,12 @@ package com.echo.test;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -9,10 +15,11 @@ public class ChannelSwitchingTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		
 	}
 
 	@Test
-	public void test() {
+	public void getChannel_should_return_channel_number_for_valid_input() {
+		
 	}
-
 }
