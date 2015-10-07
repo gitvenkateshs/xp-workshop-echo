952bc3a3c11f6072b699ff2fcfef571293ce8304
madhj <madhj@MADHJ-7REBN.cisco.com>
2015-10-07 09:02:35 +0530

    Created ChannelSwitchingTest.java

---
+++ EchoApache/test/com/echo/test/ChannelSwitchingTest.java
@@ -0,0 +1,18 @@
+package com.echo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ChannelSwitchingTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
